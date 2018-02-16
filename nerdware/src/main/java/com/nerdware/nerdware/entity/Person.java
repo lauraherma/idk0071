6a185ac7ae2aa5e9e33dd24e9cdb1dfb1830a960
trerik <trerik@ttu.ee>
2018-02-16 17:06:23 +0200

    Connection with database, setup resume

---
+++ nerdware/src/main/java/com/nerdware/nerdware/entity/Person.java
@@ -0,0 +1,22 @@
+package com.nerdware.nerdware.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@Getter
+@Setter
+public class Person {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person_seq")
+    @SequenceGenerator(name = "person_seq", sequenceName = "person_seq", allocationSize = 1)
+    private Long personId;
+}
