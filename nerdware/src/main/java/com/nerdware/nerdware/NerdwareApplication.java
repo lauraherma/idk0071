d49e3619897d51d47a25ce61ad1fad95f5ed2c22
trerik <trerik@ttu.ee>
2018-02-16 15:54:57 +0200

    Beginning of setup

---
+++ nerdware/src/main/java/com/nerdware/nerdware/NerdwareApplication.java
@@ -0,0 +1,12 @@
+package com.nerdware.nerdware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdwareApplication.class, args);
+	}
+}
