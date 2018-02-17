d9a0deaf132ac9337ac279cd53c43fac2083edc5
Laura Ernits <laerni@ttu.ee>
2018-02-17 11:57:05 +0200

    Adding color classes

---
+++ nerdware/src/main/java/com/nerdware/nerdware/entity/Color.java
@@ -0,0 +1,20 @@
+package com.nerdware.nerdware.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Color {
+    @Id
+    @Column
+    private Long colorId;
+    private String code;
+    private Double amount;
+
+}
