1a3784a15dd5703c06085fdf2c13a8ecfae105cf
laherm <laherm@ttu.ee>
2018-02-18 14:56:07 +0200

    hydrogen table

---
+++ nerdware/src/main/java/com/nerdware/nerdware/entity/hydrogen.java
@@ -0,0 +1,19 @@
+package com.nerdware.nerdware.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class hydrogen {
+    @Id
+    @Column
+    private Long id;
+    private Integer type;
+    private Double amount;
+}
