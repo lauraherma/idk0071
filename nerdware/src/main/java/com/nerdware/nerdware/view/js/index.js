c7c9f8baf1c6deb7ac397f1ba0a32f3325dc6c82
Laura Ernits <laerni@ttu.ee>
2018-03-02 13:02:58 +0200

    Log in view and showing time at the calendar view

--- nerdware/src/main/java/com/nerdware/nerdware/view/js/index.js
+++ nerdware/src/main/java/com/nerdware/nerdware/view/js/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import '../css/index.css';
 import App from '../../src/main/java/com/nerdware/view/js/App.js';
 import registerServiceWorker from '../../src/main/java/com/nerdware/nerdware/view/js/registerServiceWorker.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker();*/
+
+
