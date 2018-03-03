ccfd195befcf214d83dd709113eca7b40c8de922
laherm <laherm@ttu.ee>
2018-03-03 16:56:11 +0200

    Timeslot range

--- src/frontend/components/hairdresser/Hairdresser.js
+++ src/frontend/components/hairdresser/Hairdresser.js
@@ -1,45 +1,62 @@
 import React from 'react';
 import "./Hairdresser.css";
 import moment from "moment";
+import lodash from 'lodash';
 
 export class Hairdresser extends React.Component {
     state = {
-        times: [],
+        timeSlots: [],
     }
 
     componentDidMount() {
-        const times = [];
+        console.log(this.props);
+        const timeSlots = [];
         for (let i = 0; i < 26; i++) {
-            times.push(i);
+            const halfHourInMinutes = 30;
+            const timeSlot = moment()
+                .startOf('day')
+                .set('hour', 8)
+                .add(i * halfHourInMinutes, 'minute')
+            timeSlots.push(timeSlot);
         }
         this.setState({
             ...this.state,
-            times: times,
+            timeSlots: timeSlots,
         });
+    }
 
+    getHairdresser() {
+        return this.props.hairdresser;
     }
 
     getTimes() {
-        const times = this.state.times.map(time => {
-            const halfHourInMinutes = 30;
-            const timeslot = moment().startOf('day')
-                .set('hour', 8)
-                .add(time * halfHourInMinutes, 'minute');
-
-            return <div className="time">
-                {timeslot.format("HH:mm")}
+        const times = this.state.timeSlots.map(timeSlot => {
+            const classes = ['time'];
+            const isActive = this.getHairdresser()
+                .appointments
+                .filter(appointment => {
+                    return moment()
+                        .range(appointment.startTime, appointment.endTime)
+                        .contains(timeSlot);
+                })
+                .length > 0;
+            if (isActive) {
+                classes.push('active');
+            }
+            return <div key={timeSlot} className={classes.join(' ')}>
+                {timeSlot.format("HH:mm")}
             </div>
         });
+
         return <div className="times">
             {times}
         </div>
-
     }
 
     render() {
         return <div className="Hairdresser">
             <div className="name">
-                {this.props.hairdresser.name}
+                {this.getHairdresser().name}
             </div>
             {this.getTimes()}
         </div>
