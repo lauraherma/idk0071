a79ad9ce7db19ab2fb3919e566f06d753ab78b81
laherm <laherm@ttu.ee>
2018-04-09 21:33:05 +0300

    Changed states

--- src/frontend/components/AppointmentModal/AppointmentModal.js
+++ src/frontend/components/AppointmentModal/AppointmentModal.js
@@ -12,6 +12,7 @@ export class AppointmentModal extends React.Component {
     dataService = new DataService();
 
     state = {
+        isLoading: false,
         modal: false,
         firstName: '',
         lastName: '',
@@ -26,6 +27,7 @@ export class AppointmentModal extends React.Component {
         allWorks: [],
         workTypes: [],
         checkedWorkTypes: [],
+        options:[],
         colorRecipe: {
             id: 1,
             parts: [
