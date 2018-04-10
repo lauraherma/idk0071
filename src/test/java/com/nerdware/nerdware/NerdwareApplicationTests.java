9f20d2c7a803abec2d1989fb45d068ca3e451059
Laura Ernits <laerni@ttu.ee>
2018-04-10 11:37:34 +0300

    Now we can test our application

--- src/test/java/com/nerdware/nerdware/NerdwareApplicationTests.java
+++ src/test/java/com/nerdware/nerdware/NerdwareApplicationTests.java
@@ -1,13 +1,17 @@
 package com.nerdware.nerdware;
 
 import com.nerdware.nerdware.controller.RoleController;
+import com.nerdware.nerdware.entity.Appointment;
 import com.nerdware.nerdware.entity.Person;
 import com.nerdware.nerdware.entity.Role;
 import com.nerdware.nerdware.entity.RoleType;
+import com.nerdware.nerdware.repository.*;
+import com.nerdware.nerdware.service.AppointmentService;
 import com.nerdware.nerdware.service.RoleService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,8 +19,11 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import java.util.List;
+
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
 import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -25,6 +32,9 @@ import static org.hamcrest.Matchers.contains;
 		TransactionalTestExecutionListener.class })
 public class NerdwareApplicationTests {
 
+	private AppointmentRepository appointmentRepository;
+	private AppointmentService appointmentService;
+	private Appointment appointment;
 	private RoleService roleService;
 	private Role role;
 	private Person person;
@@ -34,13 +44,14 @@ public class NerdwareApplicationTests {
 	@Before
 	public void setup() {
 		roleService = new RoleService();
+		appointmentService = new AppointmentService(appointmentRepository);
 		role = new Role();
 		person = new Person();
 		roleType = new RoleType();
 
 		role.setPerson(person);
 		role.setRoleType(roleType);
-		}
+	}
 
 	@Test
 	public void testPersonDoesNotExist() {
@@ -53,11 +64,18 @@ public class NerdwareApplicationTests {
 	@Test
 	public void testFindClient() {
 		RoleController roleController = new RoleController(roleService);
-		person.setFirstName("Test");
 		List<Role> actual = roleController.findAllClients();
-		assertEquals(actual, contains(role));
+		assertThat(actual, contains(role));
 	}
 
+
+	@Test
+	public void testAddAppointment() {
+		appointment.getWork().setId(null);
+		Long id  = appointment.getWork().getId();
+
+		assertEquals(null, !appointmentService.appointmentDoesNotExist(id));
+	}
 
 
 }
